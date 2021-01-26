3a42c78b62a8e5080eecf27ee339e8bb5b4f55a9
awizy63 <awizy0609@gmail.com>
2021-01-26 12:29:05 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.main}>
+      <View style={styles.header}>
+        <Text style={styles.current_day}>Sun Jan 13 2019</Text>
+        <View style={styles.current_weather_data}>
+          <View style={styles.current_weather_data_value_left}>
+            <Text style={styles.current_temp}>19°</Text>
+            <Text style={styles.current_temp_feel}>12°</Text>
+          </View>
+          <View style={styles.current_weather_data_value_right}>
+            <Image style={styles.current_weather_icon} source={{ uri: 'https://img.icons8.com/ios/452/cloud.png' }} />
+            <Text style={styles.current_weather}>Cloud</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
+  },
+  header: {
+    paddingTop: 40,
+    paddingLeft: 20,
+    paddingRight: 20,
+    height: 220,
+    backgroundColor: '#1045a0',
+  },
+  current_weather_data: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row',
+  },
+  current_weather_data_value_right: {
+    flex: 1,
+    flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  current_weather_data_value_left: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'flex-start'
+  },
+  current_day: {
+    fontSize: 18,
+    marginBottom: 5,
+    color: 'white',
+
+  },
+  current_weather: {
+    fontSize: 20,
+    color: 'white',
+
+  },
+  current_weather_icon: {
+    tintColor: 'white',
+    width: 100,
+    height: 100,
+
+  },
+  current_temp: {
+    fontSize: 60,
+    color: 'white',
+
+  },
+  current_temp_feel: {
+    color: 'white',
+    fontSize: 30,
+
   },
 });
