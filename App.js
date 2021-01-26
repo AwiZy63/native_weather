757bbc19b7da0f80d24620e08916b1c427e0fb84
awizy63 <awizy0609@gmail.com>
2021-01-26 17:00:32 +0100

    Commit

--- App.js
+++ App.js
@@ -1,75 +1,35 @@
-import React from 'react';
-import { StyleSheet, Image, Text, View } from 'react-native';
+import React, { useState, useEffect} from 'react';
+import CurrentWeather from './components/CurrentWeather';
+import OtherDaysWeather from './components/OtherDaysWeather';
+import { StyleSheet, Image, ScrollView, Text, View } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+
+    axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=50.4291723&lon=2.8319805&units=metric&lang=fr&exclude=minutely,hourly,alerts&appid=37bddde895b74620b111148941cfe613`)
+
+        .then(res => {
+            setData(res.data)
+        })
+}, []);
   return (
-    <View style={styles.main}>
-      <View style={styles.header}>
-        <Text style={styles.current_day}>Sun Jan 13 2019</Text>
-        <View style={styles.current_weather_data}>
-          <View style={styles.current_weather_data_value_left}>
-            <Text style={styles.current_temp}>19°</Text>
-            <Text style={styles.current_temp_feel}>12°</Text>
-          </View>
-          <View style={styles.current_weather_data_value_right}>
-            <Image style={styles.current_weather_icon} source={{ uri: 'https://img.icons8.com/ios/452/cloud.png' }} />
-            <Text style={styles.current_weather}>Cloud</Text>
-          </View>
-        </View>
+    <ScrollView style={styles.main}>
+      <CurrentWeather data={data} />
+      <View>
+        <OtherDaysWeather data={data} />
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  main: {
-  },
   header: {
     paddingTop: 40,
     paddingLeft: 20,
     paddingRight: 20,
-    height: 220,
     backgroundColor: '#1045a0',
-  },
-  current_weather_data: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-  current_weather_data_value_right: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  current_weather_data_value_left: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'flex-start'
-  },
-  current_day: {
-    fontSize: 18,
-    marginBottom: 5,
-    color: 'white',
-
-  },
-  current_weather: {
-    fontSize: 20,
-    color: 'white',
-
-  },
-  current_weather_icon: {
-    tintColor: 'white',
-    width: 100,
-    height: 100,
-
-  },
-  current_temp: {
-    fontSize: 60,
-    color: 'white',
-
-  },
-  current_temp_feel: {
-    color: 'white',
-    fontSize: 30,
-
   },
 });
